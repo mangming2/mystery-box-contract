bb9bae717b24b8d34bea030d606dff7fe5a7f626
mangming2 <leejiho35@gmail.com>
2023-08-18 18:31:48 +0900

    Update MysteryBoxGame.sol

--- contracts/MysteryBoxGame.sol
+++ contracts/MysteryBoxGame.sol
@@ -4,29 +4,17 @@ pragma solidity 0.8.19;
 
 
 abstract contract ERC20 {
-
     event Transfer(address indexed from, address indexed to, uint256 amount);
-
     event Approval(address indexed owner, address indexed spender, uint256 amount);
-
     string public name;
-
     string public symbol;
-
     uint8 public immutable decimals;
-
     uint256 public totalSupply;
-
     mapping(address => uint256) public balanceOf;
-
     mapping(address => mapping(address => uint256)) public allowance;
-
     uint256 internal immutable INITIAL_CHAIN_ID;
-
     bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
-
     mapping(address => uint256) public nonces;
-
     constructor(
         string memory _name,
         string memory _symbol,
